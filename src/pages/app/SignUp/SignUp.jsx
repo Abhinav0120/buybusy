2b93e18632e78f2a5f4f2a97075722465691e5bb
Abhinav <ajagekarajagekar@gmail.com>
2023-08-26 02:48:31 +0530

    created ui for signUp page

--- src/pages/app/SignUp/SignUp.jsx
+++ src/pages/app/SignUp/SignUp.jsx
@@ -1,10 +1,19 @@
-
+import styles from "./SignUp.module.css";
 
 function SingUp(){
     return(
-        <>
-            SingUP Page
-        </>
+        <div className={styles.signUpFormContainer}>
+        <form className={styles.signUpForm}>
+            <h2 className={styles.title}>
+                Sign Up
+            </h2>
+            <input type="text" name="name" className={styles.signUpInput} placeholder="Enter Name"/>
+            <input type="email" name="email" className={styles.signUpInput} placeholder="Enter Email"/>
+            <input type="password" name="password" className={styles.signUpInput} placeholder="Enter Password"/>
+            <button className={styles.signUpButton}>Sign In</button>
+        </form>
+
+       </div>
     )
 }
 
