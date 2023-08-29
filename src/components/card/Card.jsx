0ea29b3b73875598995b491d78ac9a71f16ecc5b
Abhinav <ajagekarajagekar@gmail.com>
2023-08-30 03:58:24 +0530

    fetched cart data from db and displayed it in cart page

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,53 @@
 import styles from "./Card.module.css";
+import useAuthContext from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
+import { db } from "../../firebaseInit"; 
+
+
 function Card({product}){
+    const {currentUser} = useAuthContext();
+    const navigate = useNavigate();
+
+    const handleAddToCart = async (product)=>{
+        if(currentUser === null){
+            navigate("/login");
+            return;
+        }
+        // here i want to add the product to the usersCart
+        try {
+            const cartRef = doc(db, "carts", currentUser.uid);
+            const cartSnapshot = await getDoc(cartRef);
+        
+            if (cartSnapshot.exists()) {
+              const cartData = cartSnapshot.data();
+              const existingCartItem = cartData.items.find(item => item.product.id === product.id);
+        
+              if (existingCartItem) {
+                // Update the quantity if the product already exists in the cart
+                const updatedItems = cartData.items.map(item =>
+                  item.product.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+                );
+                
+                await setDoc(cartRef, { items: updatedItems }, { merge: true });
+              } else {
+                // Add the product to the cart if it doesn't exist
+                const updatedItems = [...cartData.items, { product, quantity: 1 }];
+                await setDoc(cartRef, { items: updatedItems }, { merge: true });
+              }
+            } else {
+              // Create a new cart if it doesn't exist
+              await setDoc(cartRef, {
+                userId: currentUser.uid,
+                items: [{ product, quantity: 1 }],
+              });
+            }
+        
+            console.log("Product added to cart.");
+          } catch (error) {
+            console.error("Error adding product to cart:", error);
+          }
+    } 
     return(
         <>
             <div className={styles.productContainer}>
@@ -15,7 +63,8 @@ function Card({product}){
                     <div className={styles.productPrice}>
                         <p>&#x20b9; {product.price}</p>
                     </div>
-                    <button className={styles.addToCartButton} title="Add to Cart">
+                    <button className={styles.addToCartButton} title="Add to Cart"
+                            onClick={()=>handleAddToCart(product)}>
                         Add to Cart
                     </button>
                 </div>
--- src/pages/app/cart/Cart.jsx
+++ src/pages/app/cart/Cart.jsx
@@ -1,14 +1,45 @@
 import styles from "./Cart.module.css";
 import Card from "../../../components/card/Card";
 import CartAside from "../../../components/cartAside/CartAside";
+import useAuthContext from "../../../context/AuthContext";
+import { useEffect, useState } from "react";
+import { collection, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { db } from "../../../firebaseInit";
+
 
 function Cart(){
+    const [cart, setCart] = useState([]);
+    const {currentUser} = useAuthContext();
+    useEffect(()=>{
+        const unsub = onSnapshot(doc(db, "carts", currentUser.uid), (snapshot) => {
+            // const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+            // console.log(source, " data: ", doc.data());
+            const source = snapshot.metadata.hasPendingWrites ? "Local" : "Server";
+            // console.log(source, " data: ", snapshot.data());
+            // console.log(snapshot.data().items);
+            const data = snapshot.data();
+            console.log(data.items);
+            
+            setCart(data.items);
+        });
+        return () => unsub();
+
+    },[])
+
+    console.log(cart);
+
     return(
         <>
             <div className={styles.cartPageContainer}>
                 <CartAside/>
                 <div className={styles.productContainer}>
-                    <Card/>
+                    {cart.map((item)=>{
+                        const product = item.product;
+                        const quantity = item.quantity;
+                        return(
+                            <Card product={product} key={product.docId} quantity={quantity}/>
+                        )
+                    })}
                 </div>
             </div>
         </>
