e9bf2f1969f20c8b2128f5391b2fc8beb5a9542f
Abhinav <ajagekarajagekar@gmail.com>
2023-09-02 03:29:15 +0530

    implemented serch by name and priceFilter but having some issue with categoryFilter

--- src/components/homeAside/Aside.jsx
+++ src/components/homeAside/Aside.jsx
@@ -1,36 +1,59 @@
 import styles from "./Aside.module.css"
 
-function Aside(){
+function Aside({
+    priceFilter,
+    onPriceFilterChange,
+    categoryFilters,
+    onCategoryFilterChange,
+}){
     return(
         <>
             <aside className={styles.filterContainer}>
                 <h2>Filter</h2>
                 <form>
-                    <label htmlFor="price">Price: 0000</label>
+                    <label htmlFor="price">&#x20b9; {priceFilter}</label>
                     <input  type="range" 
                             id="price" 
                             name="price" 
                             min="1" 
-                            max="100000"
+                            max="1000"
                             step="10"
-                            defaultValue="75000"
-                            className={styles.filterPriceRange}/>
+                            value={priceFilter}
+                            className={styles.filterPriceRange}
+                            onChange={onPriceFilterChange}/>
                     <h2>Category</h2>
                     <div className={styles.filterCategoryContainer}>
                         <div>
-                            <input type="checkbox" id="mensFashion" name="mensFashion"/>
+                            <input  type="checkbox" 
+                                    id="mensFashion" 
+                                    name="mensFashion"
+                                    checked={categoryFilters.mensFashion}
+                                    onChange={onCategoryFilterChange}/>
                             <label htmlFor="mensFashion">Men's Clothing</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="womensFashion" name="womensFashion"/>
+                            <input  type="checkbox"
+                                    id="womensFashion"
+                                    name="womensFashion"
+                                    checked={categoryFilters.womensFashion}
+                                    onChange={onCategoryFilterChange}
+                                    />
                             <label htmlFor="womensFashion">Women's Clothing</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="jewelery" name="jewelery"/>
+                            <input  type="checkbox" 
+                                    id="jewelery"
+                                    name="jewelery"
+                                    checked={categoryFilters.jewelery}
+                                    onChange={onCategoryFilterChange}/>
                             <label htmlFor="jewelery">Jewelery</label>
                         </div>
                         <div>
-                            <input type="checkbox" id="electronics" name="electronics"/>
+                            <input  type="checkbox" 
+                                    id="electronics"
+                                    name="electronics"
+                                    checked={categoryFilters.electronics}
+                                    onChange={onCategoryFilterChange}/>
                             <label htmlFor="electronics">Electronics</label>
                         </div>
 
--- src/pages/app/home/Home.jsx
+++ src/pages/app/home/Home.jsx
@@ -8,6 +8,17 @@ import { collection, getDocs } from "firebase/firestore";
 
 function Home({props}){
     const [products, setProduct] = useState([]);
+    const [nameFilter, setNameFilter] = useState("")
+    // const [priceFilter, setPriceFilter] = useState({ min: 0, max: 750 });
+    const [priceFilter, setPriceFilter] = useState(750);
+
+    const [categoryFilters, setCategoryFilters] = useState({
+        "men's clothing": false,
+        "women's clothing": false,
+        jewelery: false,
+        electronics: false,
+    });
+
     useEffect(()=>{
         async function fetchProducts(){
 
@@ -23,19 +34,77 @@ function Home({props}){
         fetchProducts();
     },[]);
 
+    // Function to handle name search query changes
+    const handleNameInputChange = (event) => {
+        setNameFilter(event.target.value);
+    };
+
+    // function to handle price filter changes
+    const handlePriceFilterChange = (event) =>{
+        setPriceFilter(Number(event.target.value));
+    }
+
+    const handleCategoryFilterChange = (event)=>{
+        const categoryName = event.target.name;
+        setCategoryFilters({
+            ...categoryFilters,
+            [categoryName]: event.target.checked,
+        })
+    }
+
+    const filteredProducts = products
+    .filter((product) => {
+        // Filter by search query
+        if (nameFilter.trim() === "") {
+        return true; // No search query, keep all products
+        }
+
+        // Filter by product name
+        return product.title.toLowerCase().includes(nameFilter.toLowerCase());
+    })
+    .filter((product) => {
+        // Filter by price
+        // return product.price <= priceFilter.max;
+        return product.price <= priceFilter;
+
+    })
+    // .filter((product) => {
+    //     // Filter by category
+    //     return (
+    //         (categoryFilters["men's clothing"] && product.category === "men's clothing") ||
+    //         (categoryFilters["women's clothing"] && product.category === "women's clothing") ||
+    //         (categoryFilters.jewelery && product.category === "jewelery") ||
+    //         (categoryFilters.electronics && product.category === "electronics")
+    //       );
+    // });
+
+  
+
+
     console.log("products:",products);
+    console.log("filteredProduct",filteredProducts);
+    console.log("priceFilter:", priceFilter);
+    console.log("categoryFilter:", categoryFilters);
+
 
 
     return(
         <>
         <div className={styles.homePageContainer}>
-            <Aside/>
+            <Aside  priceFilter={priceFilter}
+                    onPriceFilterChange={handlePriceFilterChange}
+                    categoryFilters={categoryFilters}
+                    onCategoryFilterChange={handleCategoryFilterChange}/>
+
             <form className={styles.homePageSearchFrom}>
-                    <input type="search" placeholder="Search By Name" className={styles.homePageSearchInput}/>
+                    <input  type="search" 
+                            placeholder="Search By Name" 
+                            className={styles.homePageSearchInput}
+                            onChange={handleNameInputChange}/>
             </form>
             <div className={styles.productList}>
                 {
-                    products.map((product)=><Card product={product} key={product.docId} visiblePage="home"/>)
+                    filteredProducts.map((product)=><Card product={product} key={product.docId} visiblePage="home"/>)
                 }
             </div>
         </div>
